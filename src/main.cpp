320f1b8c5c96ef5cd8e4b2060f62cbdba38f0171
Keoma Buselli <busellik1@gmail.com>
2025-09-21 18:24:09 +0200

    fix: SFML build and draw first snake head

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 #include <optional>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800u, 600u}), "SFML Test");
+    sf::RenderWindow window(sf::VideoMode({800u, 600u}), "Snake Game");
+    window.setFramerateLimit(60);
+
+    sf::RectangleShape snakeHead({20.f, 20.f});
+    snakeHead.setFillColor(sf::Color::Green);
+    snakeHead.setPosition({400.f, 300.f});
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -11,6 +16,7 @@ int main() {
         }
 
         window.clear(sf::Color::Black);
+        window.draw(snakeHead);
         window.display();
     }
 
